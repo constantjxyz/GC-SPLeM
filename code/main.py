5164bc0e51efdc757e9bfc589ea90c44949e9f2a
constantjxyz <44999826+constantjxyz@users.noreply.github.com>
2022-12-01 13:20:41 +0800

    main change
    main.py change

--- code/main.py
+++ code/main.py
@@ -1,11 +1,3 @@
-'''
-Author: error: git config user.name && git config user.email & please set dead value or install git
-Date: 2022-02-11 15:53:47
-LastEditors: error: git config user.name && git config user.email & please set dead value or install git
-LastEditTime: 2022-09-06 19:00:38
-FilePath: /mtmcat/main.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 
 # import dependencies from module
 import argparse
